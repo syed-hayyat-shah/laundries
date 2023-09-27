ffed85cdb67225b239356c59db4c83d71236d05d
syed-hayyat-shah <hayyat.shah@secomind.ai>
2023-09-27 11:01:45 +0500

    first commit

---
+++ javascript.js
@@ -0,0 +1,53 @@
+function darkMode() {
+  var element = document.body;
+  element.classList.toggle("dark-mode");
+}
+
+////Sign Up
+
+{/* <style>
+    .center {
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        margin: auto;
+    }
+</style> */}
+
+
+
+//  <li>
+// <a href="#" class="nav-link  nav-items-color-style  
+// ">Pages</a>
+
+// </li> 
+
+//  <li id="menu-item-24"
+// class="menu-item menu-item-type-post_type menu-item-object-page current-menu-ancestor current-menu-parent current_page_parent current_page_ancestor menu-item-has-children dropdown nav-item nav-item-24">
+// <a href="#" class="nav-link nav-items-color-style active dropdown-toggle" data-bs-toggle="dropdown"
+//   aria-haspopup="true" aria-expanded="false">Pages</a>
+// <ul class="dropdown-menu depth_0">
+//   <li id="menu-item-25"
+//     class="menu-item menu-item-type-post_type menu-item-object-page nav-item nav-item-25"><a
+//       href="blog.html" class="dropdown-item ">Blog page</a></li>
+//   <li id="menu-item-28"
+//     class="menu-item menu-item-type-post_type menu-item-object-page current-menu-item page_item page-item-16 current_page_item nav-item nav-item-28">
+//     <a href="element.html" class="dropdown-item">Elements
+//       page</a>
+//   </li>
+//   <li id="menu-item-26"
+//     class="menu-item menu-item-type-post_type menu-item-object-page nav-item nav-item-26"><a
+//       href="landing.html" class="dropdown-item ">Landing
+//       page</a></li>
+//   <li id="menu-item-29"
+//     class="menu-item menu-item-type-post_type menu-item-object-page nav-item nav-item-29"><a
+//       href="404.html" class="dropdown-item ">404 page</a></li>
+// </ul>
+// </li> 
+
+//  <li><a href="contact.html" class="nav-link  nav-items-color-style ">Contact</a></li> 
+
+
+
